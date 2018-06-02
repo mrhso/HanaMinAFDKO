a12578f4f6ed10bd4b7827fcc7a9d4310c3be015
mrhso <liu.songquan@live.cn>
2018-06-02 18:18:13 +0800

    AFDKO 2.7.1rc

--- appveyor.install.bat
+++ appveyor.install.bat
@@ -2,6 +2,8 @@ setlocal
 
 set nocache=0
 set PATH=C:\Python27-x64;%PATH%
+set AFDKO_WHL=https://img.vim-cn.com/d7/9810f51343527feda6e1eb5cac5a82bb806771.zip
+set AFDKO_VER=2.7.1rc0
 
 if not exist afdko.cached set nocache=1
 if not exist C:\Python27-x64 set nocache=1
@@ -11,7 +13,8 @@ git submodule init
 git submodule update perl-scripts
 
 if %nocache% == 1 (
-	pip install afdko
+	curl -o afdko-%AFDKO_VER%-cp27-cp27m-win_amd64.whl %AFDKO_WHL%
+	pip install afdko-%AFDKO_VER%-cp27-cp27m-win_amd64.whl
 	date /t > afdko.cached & time /t >> afdko.cached
 	echo.
 	echo AFDKO installed.
