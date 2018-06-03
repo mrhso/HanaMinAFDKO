7182f87e66b7324226ce0619113debac08610003
mrhso <liu.songquan@live.cn>
2018-06-03 11:21:50 +0800

    异常中断

--- appveyor.install.bat
+++ appveyor.install.bat
@@ -4,7 +4,7 @@ set PATH=C:\MinGW\msys\1.0\bin;%PATH%
 set OTFCC_ZIP=https://img.vim-cn.com/70/9ef4472faa2773fa29aefa19d0303063f7cc5f.zip
 set OTFCC_VER=0.10.3
 
+pip install afdko
 curl -o otfcc-win64-%OTFCC_VER%.zip %OTFCC_ZIP%
 unzip -d otfcc otfcc-win64-%OTFCC_VER%.zip
-pip install afdko
-npm install -g otfcc-c2q
+call npm install -g otfcc-c2q
--- build-tt.bat
+++ build-tt.bat
@@ -1,26 +1,26 @@
 @echo off
 
 otfccdump HanaMinA.otf -o HanaMinA.otd
-otfcc-c2q < HanaMinA.otd > HanaMinA.ttd
+call otfcc-c2q < HanaMinA.otd > HanaMinA.ttd
 otfccbuild HanaMinA.ttd -o HanaMinA.ttf
 otfccdump HanaMinB.otf -o HanaMinB.otd
-otfcc-c2q < HanaMinB.otd > HanaMinB.ttd
+call otfcc-c2q < HanaMinB.otd > HanaMinB.ttd
 otfccbuild HanaMinB.ttd -o HanaMinB.ttf
 otfccdump HanaMinC.otf -o HanaMinC.otd
-otfcc-c2q < HanaMinC.otd > HanaMinC.ttd
+call otfcc-c2q < HanaMinC.otd > HanaMinC.ttd
 otfccbuild HanaMinC.ttd -o HanaMinC.ttf
 otfccdump HanaMinI.otf -o HanaMinI.otd
-otfcc-c2q < HanaMinI.otd > HanaMinI.ttd
+call otfcc-c2q < HanaMinI.otd > HanaMinI.ttd
 otfccbuild HanaMinI.ttd -o HanaMinI.ttf
 otfccdump HanaMinExA1.otf -o HanaMinExA1.otd
-otfcc-c2q < HanaMinExA1.otd > HanaMinExA1.ttd
+call otfcc-c2q < HanaMinExA1.otd > HanaMinExA1.ttd
 otfccbuild HanaMinExA1.ttd -o HanaMinExA1.ttf
 otfccdump HanaMinExA2.otf -o HanaMinExA2.otd
-otfcc-c2q < HanaMinExA2.otd > HanaMinExA2.ttd
+call otfcc-c2q < HanaMinExA2.otd > HanaMinExA2.ttd
 otfccbuild HanaMinExA2.ttd -o HanaMinExA2.ttf
 otfccdump HanaMinExB.otf -o HanaMinExB.otd
-otfcc-c2q < HanaMinExB.otd > HanaMinExB.ttd
+call otfcc-c2q < HanaMinExB.otd > HanaMinExB.ttd
 otfccbuild HanaMinExB.ttd -o HanaMinExB.ttf
 otfccdump HanaMinExC.otf -o HanaMinExC.otd
-otfcc-c2q < HanaMinExC.otd > HanaMinExC.ttd
+call otfcc-c2q < HanaMinExC.otd > HanaMinExC.ttd
 otfccbuild HanaMinExC.ttd -o HanaMinExC.ttf
